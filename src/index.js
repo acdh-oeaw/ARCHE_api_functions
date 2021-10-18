e6a8d07169ff4736a7c82440d21ce7454bc180b5
Daniel_Stoxreiter <daniel.stoxreiter@oeaw.ac.at>
2021-10-18 10:47:26 +0200

    adding new function for opening files of directory

--- src/index.js
+++ src/index.js
@@ -10,6 +10,21 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 https://github.com/node-fetch/node-fetch*/
 
+module.exports.openDir = async(filepath, callback) => {
+    const options = {
+        method: 'GET'
+    };
+    try {
+        const response = await fetch(filepath, options);
+        console.log("statusCode:", response.statusCode);
+        console.log("headers:", response.headers);
+        const body = await response.text();
+        return callback(body);
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
 module.exports.ARCHEdownloadResourceIdM = async(host, resourceId, format, readMode, callback) => {   
     let url = host + '/' + resourceId + '/' + `metadata?format=${format}&readMode=${readMode}`;
     const options = {
